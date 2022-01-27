9840c197fdcc5386a68927aeea473e22579faea9
jzlz <1521738423@qq.com>
2022-01-27 11:18:33 +0800

    fate:下载配置axios

--- vue-study-event/src/main.js
+++ vue-study-event/src/main.js
@@ -4,6 +4,11 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://www.liulongbin.top:3008'
+
+Vue.prototype.$http = axios
 
 Vue.use(ElementUI)
 
