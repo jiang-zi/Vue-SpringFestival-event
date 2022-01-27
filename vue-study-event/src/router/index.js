588393da045042d5e2c310cae71925bb6afe532f
jzlz <1521738423@qq.com>
2022-01-27 11:08:07 +0800

    feat:添加login和reg页面

--- vue-study-event/src/router/index.js
+++ vue-study-event/src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from '../views/login'
+import reg from '../views/reg'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  { path: '/login', component: login },
+  { path: '/reg', component: reg }
 ]
 
 const router = new VueRouter({
